6d0b1aaadfc3a72b285fdf95f3123b9563e57044
bobgoz <nikita.zaytsev2013@gmail.com>
2025-07-04 00:20:15 +0300

    3

--- backend/foodgram_final/foodgram_final/settings.py
+++ backend/foodgram_final/foodgram_final/settings.py
@@ -1,21 +1,14 @@
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
 
-load_dotenv()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
